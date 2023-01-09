d409f97549e81532da0944958241986b32e3b043
gautamjain09 <gautamjain0918@gmail.com>
2023-01-10 02:31:12 +0530

    Comment Modal

--- lib/controllers/comment_controller.dart
+++ lib/controllers/comment_controller.dart
@@ -2,5 +2,15 @@ import 'package:get/get.dart';
 import 'package:snapshorts_app/models/comment_modal.dart';
 
 class CommentController extends GetxController {
-  final Rx<List<Comment>> _commentList = Rx<List<Comment>>([]);
+  //private member
+  final Rx<List<Comment>> _commentsList = Rx<List<Comment>>([]);
+
+  // getter
+  List<Comment> get commentsList => _commentsList.value;
+
+  updateVideoData() {}
+
+  postComment(String commentText) async {}
+
+  getComments() async {}
 }
--- lib/models/comment_modal.dart
+++ lib/models/comment_modal.dart
@@ -1,10 +1,44 @@
-import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class Comment extends StatelessWidget {
-  const Comment({Key? key}) : super(key: key);
+class Comment {
+  String username;
+  String commentText;
+  final datePublished;
+  List likes;
+  String profilePhoto;
+  String uid;
+  String videoId;
 
-  @override
-  Widget build(BuildContext context) {
-    return Container();
+  Comment({
+    required this.username,
+    required this.commentText,
+    required this.datePublished,
+    required this.likes,
+    required this.profilePhoto,
+    required this.uid,
+    required this.videoId,
+  });
+
+  Map<String, dynamic> toJson() => {
+        'username': username,
+        'commentText': commentText,
+        'datePublished': datePublished,
+        'likes': likes,
+        'profilePhoto': profilePhoto,
+        'uid': uid,
+        'videoId': videoId,
+      };
+
+  static Comment fromSnap(DocumentSnapshot snap) {
+    var snapshot = snap.data() as Map<String, dynamic>;
+    return Comment(
+      username: snapshot['username'],
+      commentText: snapshot['commentText'],
+      datePublished: snapshot['datePublished'],
+      likes: snapshot['likes'],
+      profilePhoto: snapshot['profilePhoto'],
+      uid: snapshot['uid'],
+      videoId: snapshot['videoId'],
+    );
   }
 }
